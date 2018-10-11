2f4e83b1986d9bb67dc335c1898cd905f4ab9f87
misha gupta <guptamisha88@gmail.com>
2018-10-11 14:40:13 +0530

    added code to repository
    added code to repository

---
+++ TTGarmentAdmin/Models/VehicleDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TTGarmentAdmin.Models
+{
+    public class VehicleDetail
+    {
+        public string Id { get; set; }
+        public string Name{ get; set; }
+        public string MobileNo{ get; set; }
+        public string Address{ get; set; }
+        public string ProfilePictureUrl{ get; set; }
+        public int CItyId { get; set; }
+        public string CityName { get; set; }
+        public int StateId { get; set; }
+        public string StateName { get; set; }
+        public string PinCode { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string AutoVehicleNo { get; set; }
+        public double BalancePoints { get; set; }
+        public string NfcCardNo { get; set; }
+    }
+}
